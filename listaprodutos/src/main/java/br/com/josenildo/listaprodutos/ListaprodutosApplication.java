7744e58638037a0e22e6cdbb1b9c055060954278
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2023-09-27 00:19:42 -0300

    Start: Criar projeto base da API
    Criado um projeto base com as dependencias Spring Web, Data JPA, Postgre Driver e Validation para o desenvolvimento de uma API de produtos com Spring Boot 3 e Java 17

---
+++ listaprodutos/src/main/java/br/com/josenildo/listaprodutos/ListaprodutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.josenildo.listaprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaprodutosApplication.class, args);
+	}
+
+}
---
+++ listaprodutos/src/test/java/br/com/josenildo/listaprodutos/ListaprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josenildo.listaprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
