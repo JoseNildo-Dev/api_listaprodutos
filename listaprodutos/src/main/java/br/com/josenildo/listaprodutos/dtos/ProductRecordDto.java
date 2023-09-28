79e48d258164666be0fc58aa0d92d7f31631a25f
José Nildo de Macedo Pereira <jnmpdev@gmail.com>
2023-09-28 00:22:41 -0300

    Start: Implementar API REST de gerenciamento de produtos
    Implementado uma pequena API REST com metodos CRUD, que realiza o cadastro de produtos (com os dados name (string) e value (float)), usando Spring Web, JPA e PostgreSQL

---
+++ listaprodutos/src/main/java/br/com/josenildo/listaprodutos/dtos/ProductRecordDto.java
@@ -0,0 +1,14 @@
+package br.com.josenildo.listaprodutos.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+
+
+
+
+
+}
